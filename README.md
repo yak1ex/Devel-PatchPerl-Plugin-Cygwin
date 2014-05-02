8ad5f24e1b871edf281c2c434abf6ac183634d1e
Yasutaka ATARASHI <yakex@cpan.org>
2014-05-02 22:40:55 +0900

    Just add patch for 5.19.8.

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Devel::PatchPerl::Plugin::Cygwin - Devel::PatchPerl plugin for Cygwin
+
+# VERSION
+
+version v0.0.1
+
+# SYNOPSIS
+
+# DESCRIPTION
+
+# AUTHOR
+
+Yasutaka ATARASHI <yakex@cpan.org>
+
+# COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Yasutaka ATARASHI.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
--- lib/Devel/PatchPerl/Plugin/Cygwin.pm
+++ lib/Devel/PatchPerl/Plugin/Cygwin.pm
@@ -3,8 +3,61 @@ package Devel::PatchPerl::Plugin::Cygwin;
 use strict;
 use warnings;
 
-# ABSTRACT:
+# ABSTRACT: Devel::PatchPerl plugin for Cygwin
 # VERSION
+
+use File::pushd qw[pushd];
+use File::Spec;
+
+my @patch = (
+	{
+		perl => [ qr/^5\.19\.8$/ ],
+		subs => [ [ \&_patch_cygwin_c_stdio ] ],
+	}
+);
+
+sub patchperl
+{
+	return unless $^O eq 'cygwin';
+
+	require Devel::PatchPerl;
+# Devel::PatchPerl::_patch(),_is() are NOT a public interface, so the existence SHOULD NOT be relied
+	if(! defined *Devel::PatchPerl::_patch{CODE} || ! defined *Devel::PatchPerl::_is{CODE}) {
+		die 'Devel::PatchPerl::_patch() or Devel::PatchPerl::_is() not found, please contact with the author of '.__PACKAGE__;
+	}
+
+	shift if eval { $_[0]->isa(__PACKAGE__) };
+	my (%args) = @_;
+# Copy from Devel::PatchPerl::patch_source()
+	my $source = File::Spec->rel2abs($args{source});
+	{
+		my $dir = pushd( $source );
+		for my $p ( grep { Devel::PatchPerl::_is( $_->{perl}, $args{version} ) } @patch ) {
+			for my $s (@{$p->{subs}}) {
+				my($sub, @args) = @$s;
+				push @args, $args{version} unless scalar @args;
+				$sub->(@args);
+			}
+		}
+	}
+}
+
+sub _patch_cygwin_c_stdio
+{
+	Devel::PatchPerl::_patch(<<'END');
+--- cygwin/cygwin.c.orig        2014-01-13 09:20:07.000000000 +0900
++++ cygwin/cygwin.c     2014-05-02 19:16:25.950179100 +0900
+@@ -2,6 +2,7 @@
+  * Cygwin extras
+  */
+
++#define PERLIO_NOT_STDIO 0
+ #include "EXTERN.h"
+ #include "perl.h"
+ #undef USE_DYNAMIC_LOADING
+END
+}
+
 
 1;
 __END__
